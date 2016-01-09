682e3e38c409717e2a397a443eaf63a5a604702e
G. Takanishi <gtakanishi@Gs-MacBook-Pro.local>
2016-01-08 19:45:07 -1000

    fxns first exercise

---
+++ functions.js
@@ -0,0 +1,92 @@
+var a = 5;
+var b =  12;
+function add(a, b) {
+	return a + b;
+
+}
+var sum=add(a,b);
+console.log(sum);
+
+function subtract(a, b) {
+	return b - a;
+}
+var difference = subtract(a,b);
+
+function multiply(a, b) {
+	return a * b;
+}
+var product = multiply(a,b);
+
+
+	console.log("My football team lost " + difference + " times this week");
+
+
+console.log("I CAN ADDZ " + sum + " NUMBERS");
+
+
+function checkProduct(product, difference){
+	return product * difference;
+}
+
+var prodiff = checkProduct(product, difference);
+console.log(prodiff);
+
+function addThenSubtract(a, b, product){
+	return a + b - product;
+}
+
+function addThenMultiply(b, sum, a){
+	return (b + sum) * a;
+}
+var howMany = addThenMultiply(b, sum, a);
+console.log(howMany);
+
+function createFullName(firstName, lastName){
+	return firstName + " " + lastName;
+}
+
+var fullName = createFullName(firstName, lastName);
+console.log(fullName);
+
+
+function verifyDrinkingAge(age)
+{
+	if (age < 21){
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+
+console.log (verifyDrinkingAge(21));
+
+
+function canDrinkBeer(age){
+	if (age<21){
+		console.log("The party will have tons of Cake");
+	}
+
+	else {
+		console.log("The party will have an open bar");
+	}
+}
+
+(canDrinkBeer(21));
+
+
+function createFood(firstName, lastName, food){
+	return fullName + " loves to eat " + food;
+}
+
+var eatFood = createFood("Papa", "John", "pizza");
+console.log (eatFood);
+
+
+var howMany = x;
+function printMessage()
+"Can Drink Beer"
+"Let it go"
+
+
+
