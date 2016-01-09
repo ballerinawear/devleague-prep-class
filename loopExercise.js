06d73a09fe37c8f0de302d5992623deba893a401
G. Takanishi <gtakanishi@Gs-MacBook-Pro.local>
2016-01-08 22:13:59 -1000

    loop exercise 2

--- loopExercise.js
+++ loopExercise.js
@@ -2,9 +2,25 @@
 
 
 function youGetTaco(action){
-	if(action === 'eat)'){
+	if (action === 'eat'){
 		return 'Eat Tacos';
 	}
+
+	
 }
 var myAction = 'eat';
-console.log('youGetTaco', myAction, youGetTaco(myAction));
+console.log(youGetTaco('eat'));
+
+
+//greater than
+
+var firstNumber = 3;
+var secondNumber = 4;
+function isNumberGreaterThan(a, b){
+	if (a > b) {
+		console.log("true");
+	} else {
+		console.log("false");
+	}
+}
+isNumberGreaterThan(firstNumber, secondNumber);
