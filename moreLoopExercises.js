333fab0222f5b85b5e6020775107735c0db88b9a
G. Takanishi <22designshi@gmail.com>
2016-01-12 22:41:10 -1000

    more loop answers

--- moreLoopExercises.js
+++ moreLoopExercises.js
@@ -155,15 +155,15 @@ var isNapTime = false;
 var napSchedule = [false, false, true, false, true, true];
 
 function nap(schedule){
-	if (schedule === true){
+	if (schedule=== true){
 			console.log("ZzZzZzZz");
 		} else {
 			console.log("Gotta get to work!");
-		}
-
+		} 
+		isNapTime = true;
+}
 	for (var i = 0; i < napSchedule.length; i++){
 		nap(napSchedule[i]);
-	}
 	
 }
 
